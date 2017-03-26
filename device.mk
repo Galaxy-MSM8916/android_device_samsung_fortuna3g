0e192f8c5e1d5b40263c8289e03c5a1226fe8f7b
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-03-26 01:45:09 -0400

    gprimelte: Move acdbs to vendor tree

--- device.mk
+++ device.mk
@@ -13,17 +13,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/gprimeltezt/overlay
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/thermal-engine-8916.conf:system/etc/thermal-engine-8916.conf
 	
-# Audio configuration
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/audio/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
-	$(LOCAL_PATH)/audio/General_cal.acdb:system/etc/General_cal.acdb \
-	$(LOCAL_PATH)/audio/Global_cal.acdb:system/etc/Global_cal.acdb \
-	$(LOCAL_PATH)/audio/Handset_cal.acdb:system/etc/Handset_cal.acdb \
-	$(LOCAL_PATH)/audio/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
-	$(LOCAL_PATH)/audio/Headset_cal.acdb:system/etc/Headset_cal.acdb \
-	$(LOCAL_PATH)/audio/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
-	$(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-
 # Media configurations
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/media/media_codecs_8929.xml:system/etc/media_codecs_8929.xml \
