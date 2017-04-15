265f0d6ecba0766426e7887e4b4e9f2f04c906fe
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-15 14:52:45 -0400

    gprimelte: Update kernel variant config name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ TARGET_OTA_ASSERT_DEVICE := gprimeltezt,fortunalte,fortunaltezt,fortunalteuh,gpr
 TARGET_UNIFIED_DEVICE := true
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_aio_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_defconfig
 
 # Partition sizes
 BOARD_SYSTEMIMAGE_PARTITION_SIZE    :=  1468006400
