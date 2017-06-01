560f389bd451693a8835f25400b17d86c61d0770
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-06-01 18:08:29 -0400

    system_prop: Add screen density property

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=0
 
+# LCD
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=240
+
 # RAM
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=false \
