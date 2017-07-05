35e69b2638e2ac381638016f42d1c836579d5c1e
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-05 23:26:25 +0200

    system_prop: Add ro.telephony.ril.config property
     * This is required for SIM card detection.
    
    Change-Id: I763965a1ecf5ea654310cabf8aa76246347b0380

--- system_prop.mk
+++ system_prop.mk
@@ -15,6 +15,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	ro.telephony.default_network=9 \
 	ro.multisim.simslotcount=2 \
+	ro.telephony.ril.config=simactivation \
 	telephony.lteOnCdmaDevice=0 \
 	persist.radio.multisim.config=dsds \
 	rild.libpath2=/system/lib/libsec-ril-dsds.so
