bcdc84198ad4cab4fc07c630f07c438cfdea8881
itexpert120 <itexpert120@gmail.com>
2020-08-08 19:15:44 +0200

    Changes for Q

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/fortuna3g
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_fortuna3g.mk \
-	$(LOCAL_DIR)/pa_fortuna3g.mk \
 	$(LOCAL_DIR)/lineage_fortuna3g.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_fortuna3g-eng \
+	lineage_fortuna3g-userdebug \
+	lineage_fortuna3g-user
--- system_prop.mk
+++ system_prop.mk
@@ -1,12 +1,3 @@
 # CABL
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=0
-
-# LCD
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=240
-
-# RAM
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=false \
-	ro.config.zram=false
